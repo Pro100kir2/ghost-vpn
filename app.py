a3eea6805f405e9987a6dd7d37f09ad27a086ba8
pro100kir2 <kirilllupanov15@gmail.com>
2024-11-20 18:16:45 +0300

    1

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ from flask_session import Session
 
 app = Flask(__name__)
 
-
 # Настройки для Flask-сессии
 app.secret_key = os.urandom(24)
 app.config['SESSION_TYPE'] = 'filesystem'  # Используем файловую систему для хранения сессий
