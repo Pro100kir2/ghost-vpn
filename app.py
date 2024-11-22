0eaa671d320279236756cd28f2c275537d320d1a
pro100kir2 <kirilllupanov15@gmail.com>
2024-11-22 14:31:14 +0300

    v2.0 capcha

--- app.py
+++ app.py
@@ -142,7 +142,60 @@ def login():
             if conn:
                 conn.close()
     return render_template('login.html')
-
+@app.route('/home', methods=['GET'])
+@login_required
+def home():
+    return render_template('home.html')
+@app.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html')
+@app.route('/tariff')
+@login_required
+def tariff():
+    return render_template('tariff.html')
+@app.route('/setting')
+@login_required
+def setting():
+    return render_template('setting.html')
+@app.route('/about')
+@login_required
+def about():
+    return render_template('about.html')
+@app.route('/my-home-profile')
+@login_required
+def my_home_profile():
+    user_id = session['user_id']
+    conn = None
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        # Получаем имя пользователя, время подписки (time) и статус
+        cur.execute("SELECT username, time, status FROM users WHERE id = %s", (user_id,))
+        user = cur.fetchone()
+        if user:
+            username, time_left, status = user
+            # Проверяем, что time >= 0, чтобы отобразить оставшееся время
+            if time_left >= 0:
+                # Рассчитываем оставшееся время как количество дней
+                remaining_time = timedelta(days=time_left)
+                status = "Активен" if remaining_time.days > 0 else "Неактивен"
+                # Форматируем оставшееся время в формате "мес:дней"
+                time_remaining = f"{remaining_time.days // 30} мес {remaining_time.days % 30} дн" if remaining_time.days > 0 else "Подписка завершена"
+            else:
+                # Если time < 0, то подписка завершена
+                time_remaining = "Подписка завершена"
+                status = "Неактивен"
+            return render_template('my-home-profile.html', username=username, time_remaining=time_remaining, status=status)
+        else:
+            flash("Пользователь не найден.", "error")
+            return redirect(url_for('profile'))
+    except Exception as e:
+        flash(f'Ошибка при извлечении данных: {str(e)}', "error")
+        return redirect(url_for('profile'))
+    finally:
+        if conn:
+            conn.close()
 # Заголовки для предотвращения кэширования
 @app.after_request
 def add_no_cache_headers(response):
